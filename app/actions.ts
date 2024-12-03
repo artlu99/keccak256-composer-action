1fddb855de7814a2a1020785498e25861ab742ac
artlu99 <alu99.sm@gmail.com>
2024-12-03 17:00:35 -0500

    unique messageHash

--- app/actions.ts
+++ app/actions.ts
@@ -56,16 +56,16 @@ export async function processSubmission(formData: FormData) {
   });
 
   try {
-    const { fid, fid2, fid3, fid4, ...filteredData } = validatedFields.data;
+    const { fid, fid2, fid3, fid4, messageHash, ...filteredData } = validatedFields.data;
 
     // Call request for each fid
-    await graphQLClient.request(mutation, { ...filteredData, fid });
+    await graphQLClient.request(mutation, { ...filteredData, messageHash, fid });
     if (fid2 !== null && fid2 > 0)
-      await graphQLClient.request(mutation, { ...filteredData, fid: fid2 });
+      await graphQLClient.request(mutation, { ...filteredData, messageHash: messageHash + '-2', fid: fid2 });
     if (fid3 !== null && fid3 > 0)
-      await graphQLClient.request(mutation, { ...filteredData, fid: fid3 });
+      await graphQLClient.request(mutation, { ...filteredData, messageHash: messageHash + '-3', fid: fid3 });
     if (fid4 !== null && fid4 > 0)
-      await graphQLClient.request(mutation, { ...filteredData, fid: fid4 });
+      await graphQLClient.request(mutation, { ...filteredData, messageHash: messageHash + '-4', fid: fid4 });
   } catch (error) {
     console.error("Error updating data:", error);
   }
